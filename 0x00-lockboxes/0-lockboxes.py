3bdfff9d68b9985c8c750fea774c3e819536a6c8
diegolopezq95 <927@holbertonschool.com>
2020-12-09 18:38:37 -0500

    [fix] pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,8 +6,7 @@ def canUnlockAll(boxes):
     Function that determines if all boxes can be opened
 
     Params:
-        boxes (list): is a list of lists 
-
+        boxes (list): is a list of lists
     Retorna:
         True if all boxes can be opened, else return False
     """
@@ -17,4 +16,3 @@ def canUnlockAll(boxes):
             if key not in jump:
                 jump.append(key)
     return True if len(jump) == len(boxes) else False
-
