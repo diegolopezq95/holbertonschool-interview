13028da13c12dd8babd2bd785bb09daa89f81f6a
diegolopezq95 <927@holbertonschool.com>
2020-12-09 18:36:58 -0500

    [feature] lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 
+
+def canUnlockAll(boxes):
+    """
+    Function that determines if all boxes can be opened
+
+    Params:
+        boxes (list): is a list of lists 
+
+    Retorna:
+        True if all boxes can be opened, else return False
+    """
+    jump = [0]
+    for idx in jump:
+        for key in boxes[idx]:
+            if key not in jump:
+                jump.append(key)
+    return True if len(jump) == len(boxes) else False
+
