b40c45dfe4c0fe9eb5fce65152624351115fceef
diegolopezq95 <927@holbertonschool.com>
2020-12-21 17:41:03 -0500

    [fix] the main builds the tree, I just had to create the node

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -8,30 +8,14 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *tmp, *new;
+	binary_tree_t *new;
 
-	tmp = parent;
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
-	if (parent == NULL)
-	{
-		new->n = value;
-		tmp = new;
-		tmp->left = NULL;
-		tmp->right = NULL;
-		return (tmp);
-	}
-	else
-	{
-		new->parent = tmp;
-		new->n = value;
-		new->left = NULL;
-		new->right = NULL;
-		if (tmp->n > value)
-			tmp->left = new;
-		else
-			tmp->right = new;
-		return (new);
-	}
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+	return (new);
 }
